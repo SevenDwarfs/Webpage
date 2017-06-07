a7e9b6f8e1c96c033a4375602c37d5ea01adf1f5
Kinpzz <kinpzzz@gmail.com>
2017-06-07 12:07:33 +0800

    add docker config

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 docker stop web-server
 docker rm web-server
 docker rmi kinpzz/web-server
-docker build -t kinpzz/web-server
+docker build -t kinpzz/web-server .
 docker run -d -p 127.0.0.1:8081:8081 --name web-server kinpzz/web-server
