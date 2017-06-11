59f2f9ee9a19cf290e1c413503a3dd4e67bab508
Kinpzz <kinpzzz@gmail.com>
2017-06-12 00:40:34 +0800

    remove dist dir before build

--- run.sh
+++ run.sh
@@ -5,4 +5,5 @@ docker rmi web-server
 docker build -t web-server .
 docker run -d --name web-server web-server
 # save to /home/kinpzz/webpage
+rm -rf /jenkins/webpage/dist
 docker cp web-server:/web-server/dist /jenkins/webpage
