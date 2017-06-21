0404cb12ce60eaf17dca25b92d0a742b43c55836
zhzdeng <zhzdeng@qq.com>
2017-06-21 20:19:45 +0800

    修复查询电影位置时提交表单错误

--- src/views/Booking.vue
+++ src/views/Booking.vue
@@ -220,7 +220,7 @@ export default {
       let date = this.$moment().add(this.timeSelect, 'days').format('YYYYMMDD')
       let time = this.timeStamp
       this.seatsLoading = true
-      Cinema.fetchSeat(this.$route.params.id, this.form.cinema.id, date, time).then(res => {
+      Cinema.fetchSeat(this.$route.cinema.id, this.form.params.id, date, time).then(res => {
         let cinema = res[0]
         this.seatid = cinema.id
         let seats = cinema.seats + ''
@@ -423,7 +423,7 @@ export default {
   }
 
   .order__op {
-  
+
   }
 
   .order__op__item {
